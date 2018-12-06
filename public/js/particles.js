dd30a71a4cb06f655fcda812fa462eb92b8c3cb2
tdar31 <t.dardani8@gmail.com>
2018-12-06 01:48:41 -0800

    major overhaul

--- public/js/particles.js
+++ public/js/particles.js
@@ -1,7 +1,7 @@
 particlesJS("particles-js", {
   particles: {
     number: {
-      value: 305,
+      value: 600,
       density: { enable: true, value_area: 1000.1416867389551 }
     },
     color: { value: "#ffffff" },
@@ -17,7 +17,7 @@ particlesJS("particles-js", {
       anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
     },
     size: {
-      value: 3,
+      value: 4,
       random: true,
       anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
     },
@@ -30,7 +30,7 @@ particlesJS("particles-js", {
     },
     move: {
       enable: true,
-      speed: 3,
+      speed: 4,
       direction: "top",
       random: true,
       straight: false,
@@ -43,14 +43,14 @@ particlesJS("particles-js", {
     detect_on: "canvas",
     events: {
       onhover: { enable: false, mode: "repulse" },
-      onclick: { enable: false, mode: "push" },
+      onclick: { enable: true, mode: "push" },
       resize: true
     },
     modes: {
       grab: { distance: 400, line_linked: { opacity: 1 } },
       bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-      repulse: { distance: 200, duration: 0.4 },
-      push: { particles_nb: 4 },
+      repulse: { distance: 100, duration: 0.4 },
+      push: { particles_nb: 10 },
       remove: { particles_nb: 2 }
     }
   },
