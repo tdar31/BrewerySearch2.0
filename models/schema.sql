b9a6d3312e7a245972eb09591834d8e2464ec1f0
tdar31 <t.dardani8@gmail.com>
2018-12-15 21:10:09 -0800

    building out schemas

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,15 @@
 DROP DATABASE IF EXISTS breweries_DB;
 CREATE DATABASE breweries_DB;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE breweries_DB;
+
+CREATE TABLE breweries (
+  id INT NOT NULL AUTO_INCREMENT,
+  brewery_name VARCHAR(100) NULL,
+  region VARCHAR(75) NULL,
+  full_address VARCHAR(250) NULL,
+  full_description VARCHAR(2500) NULL,
+  website VARCHAR(100) NULL,
+  photo VARCHAR(100) NULL,
+  PRIMARY KEY (id)
+);
---
+++ models/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO breweries (brewery_name, region, full_address, full_description, website, photo)
+VALUES ("papertowel", "consumer breweries", "10.00", "15");
+
+INSERT INTO breweries (brewery_name, region, full_address, full_description, website, photo)
+VALUES ("papertowel", "consumer breweries", "10.00", "15");
+
+INSERT INTO breweries (brewery_name, region, full_address, full_description, website, photo)
+VALUES ("papertowel", "consumer breweries", "10.00", "15");
