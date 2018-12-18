5f2a91425126b2621e803bad22817e62b91e0ddf
tdar31 <t.dardani8@gmail.com>
2018-12-17 16:59:33 -0800

    switched to using for loop

--- public/js/databaseCall.js
+++ public/js/databaseCall.js
@@ -1,10 +1,10 @@
 var myFunction = function() {
   console.log("works");
   $.get("/api", function(data) {
-    console.log(data[0].breweryName);
-    $(".breweryContainer").text(data[0].breweryName);
-    data.forEach(function(element) {
-      console.log(element);
-    });
+    console.log(data);
+    for (var i = 0; i < data.length; ++i) {
+      console.log(data[i]);
+      $(".breweryContainer").append(data[i].breweryName);
+    }
   });
 };
--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
-// var logger = require("morgan");
-// var mongoose = require("mongoose");
 
 var db = require("./models");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
-
-// Use morgan logger for logging requests
-// app.use(logger("dev"));
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
@@ -32,24 +27,11 @@ require("./routes/htmlRoutes")(app);
 
 var syncOptions = { force: false };
 
-// mongoose.connect(
-//   "mongodb://localhost/userdb",
-//   { useNewUrlParser: true }
-// );
-
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
-// if (process.env.NODE_ENV === "test") {
-//   syncOptions.force = true;
-// }
-
-// app.listen(PORT, function() {
-//   console.log(
-//     "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-//     PORT,
-//     PORT
-//   );
-// });
+if (process.env.NODE_ENV === "test") {
+  syncOptions.force = true;
+}
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
