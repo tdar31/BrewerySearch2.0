793684e7dc502c9f738aeba91b77dc0a1678e96c
tdar31 <t.dardani8@gmail.com>
2018-12-18 21:37:21 -0800

    brew generator setup

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,7 +1,7 @@
 USE breweries_DB;
 
 INSERT INTO breweries (breweryName, region, fullAddress, fullDescription, website, photo)
-VALUES ("Stoup Brewing", "Ballard", "123 Memer Street", "This is the full Description Slot", "www.meme.com", "photo address");
+VALUES ("Stoup Brewing", "Ballard", "123 Memer Street", "This is the full Description Slot", "www.meme.com", "http://via.placeholder.com/600x400");
 
 INSERT INTO breweries (breweryName, region, fullAddress, fullDescription, website, photo)
-VALUES ("Ballard Brewing", "Ballard", "432 Ave Street", "This is the full Description Slot", "www.bbbbbb.com", "photo address");
+VALUES ("Ballard Brewing", "Ballard", "432 Ave Street", "This is the full Description Slot", "www.bbbbbb.com", "http://via.placeholder.com/600x400");
--- public/js/databaseCall.js
+++ public/js/databaseCall.js
@@ -1,10 +1,40 @@
-var myFunction = function() {
-  console.log("works");
+var generateBrew = function() {
   $.get("/api", function(data) {
-    console.log(data);
+
+    // var breweryTitle;
+
     for (var i = 0; i < data.length; ++i) {
       console.log(data[i]);
-      $(".breweryContainer").append(data[i].breweryName);
+      var brew = data[i];
+      //Brewery Title
+      $("<h1>").addClass("breweryName title").appendTo(".breweryContainer"); //.text(brew.breweryName)
+      //Contact Line (Contains Image, Address and Phone)
+      // $("<div>").addClass("contactRow").appendTo(".breweryContainer"); //.text("THIS IS THE CONTACT ROW")
+      //Image
+      $("<img>").addClass("breweryImage").appendTo(".breweryContainer"); //.attr("src", brew.photo)
+      //Address
+      $("<div>").addClass("addressText").appendTo(".breweryContainer"); //.text(brew.fullAddress)
+      //Phone
+      $("<div>").addClass("phoneNum").appendTo(".breweryContainer"); //.text(brew.Phone)
+      //Region
+      $("<div>").addClass("regionText").appendTo(".breweryContainer"); //.text(brew.region)
+      //Website
+      $("<div>").addClass("websiteText").appendTo(".breweryContainer"); //.text(brew.website)
+      //Full Description
+      $("<div>").addClass("descriptText").appendTo(".breweryContainer"); //.text(brew.website)
+
     }
+
+    for (var i = 0; i < data.length; ++i) {
+      var brew = data[i];
+      $(".breweryName").eq(i).text(brew.breweryName);
+      $(".breweryImage").eq(i).attr("src", brew.photo);
+      $(".addressText").eq(i).text(brew.fullAddress);
+      $(".phoneNum").eq(i).text(brew.phone);
+      $(".regionText").eq(i).text(brew.region);
+      $(".websiteText").eq(i).text(brew.website);
+      $(".descriptText").eq(i).text(brew.fullDescription);
+    }
+
   });
 };
